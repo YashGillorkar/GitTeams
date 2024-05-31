393cb7273598b643f4e0e59e4a4902ca7da134e1
Yash Gillorkar <144929163+YashGillorkar@users.noreply.github.com>
2024-05-31 11:25:48 +0530

    Autowiring done

--- StudentManagement/src/main/java/com/application/controller/HomeController.java
+++ StudentManagement/src/main/java/com/application/controller/HomeController.java
@@ -1,8 +1,12 @@
 package com.application.controller;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.RestController;
+
+import com.application.service.StudentServiceI;
 
 @RestController
 public class HomeController {
 
+	@Autowired StudentServiceI ssi;
 }
