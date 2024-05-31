b350f386aea3f485c8d9ee0144571f8ddc5f6677
Yash Gillorkar <144929163+YashGillorkar@users.noreply.github.com>
2024-05-31 11:01:19 +0530

    Added Entity class

--- StudentManagement/src/main/java/com/application/model/Student.java
+++ StudentManagement/src/main/java/com/application/model/Student.java
@@ -1,6 +1,19 @@
 package com.application.model;
 
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+import lombok.Getter;
+import lombok.Setter;
 
+@Setter
+@Getter
+@Entity
 public class Student {
+
+	@Id
+	private int id;
+	private String name;
+	private double marks;
+	private String review;
 
 }
