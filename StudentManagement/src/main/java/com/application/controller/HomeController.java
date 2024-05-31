a1efd6db7db1bfc3941dbb83b5a5dad895ecf27b
Yash Gillorkar <144929163+YashGillorkar@users.noreply.github.com>
2024-05-31 11:14:14 +0530

    Added Annotations

--- StudentManagement/src/main/java/com/application/controller/HomeController.java
+++ StudentManagement/src/main/java/com/application/controller/HomeController.java
@@ -1,5 +1,8 @@
 package com.application.controller;
 
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
 public class HomeController {
 
 }
--- StudentManagement/src/main/java/com/application/serviceImpl/StudentServiceImpl.java
+++ StudentManagement/src/main/java/com/application/serviceImpl/StudentServiceImpl.java
@@ -1,5 +1,8 @@
 package com.application.serviceImpl;
 
+import org.springframework.stereotype.Service;
+
+@Service
 public class StudentServiceImpl {
 
 }
